c22c7a2b4e69f03fa78f5f564b6aa6c536de71f8
Weicheng Pan <weichengpan@gmail.com>
2017-03-16 15:48:39 +0800

    display userId

--- src/main/java/com/linecorp/sample/login/Application.java
+++ src/main/java/com/linecorp/sample/login/Application.java
@@ -14,7 +14,6 @@
  * under the License.
  */
 package com.linecorp.sample.login;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
